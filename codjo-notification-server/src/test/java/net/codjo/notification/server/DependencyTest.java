223a1a53de2a560acf4434833a53f13dec568391
Fabien DUMINY <fabien.duminy@allianzgi.fr>
2015-09-02 17:28:43 +0200

    Make java 8 compatible

--- codjo-notification-server/src/test/java/net/codjo/notification/server/DependencyTest.java
+++ codjo-notification-server/src/test/java/net/codjo/notification/server/DependencyTest.java
@@ -5,17 +5,18 @@ import net.codjo.test.common.depend.PackageDependencyTestCase;
  *
  */
 public class DependencyTest extends PackageDependencyTestCase {
+    private static final boolean JAVA_8 = System.getProperty("java.version").startsWith("1.8.");
 
     public void test_dependency() throws Exception {
         Dependency dependency = createDependency();
-        dependency.assertDependency("dependency.txt");
+        dependency.assertDependency(JAVA_8 ? "dependency-jdk8.txt" : "dependency.txt");
         dependency.assertNoCycle();
     }
 
 
     public void test_dependencyTest() throws Exception {
         Dependency dependency = createTestDependency();
-        dependency.assertDependency("dependencyTest.txt");
+        dependency.assertDependency(JAVA_8 ? "dependencyTest-jdk8.txt" : "dependencyTest.txt");
         dependency.assertNoCycle();
     }
 }
